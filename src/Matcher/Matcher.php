c179a57d30c50c7efdd6a9adf3a9dac0904b1343
hidenorigoto <hidenorigoto@gmail.com>
2015-06-24 22:52:49 +0900

    refactored Matcher

--- src/Matcher/Matcher.php
+++ src/Matcher/Matcher.php
@@ -15,13 +15,38 @@ namespace Quartet\Haydn\Matcher;
 class Matcher implements MatcherInterface
 {
     /**
-     * @var
+     * @var array
      */
     private $option;
+
+    /**
+     * @var array
+     */
+    private $matchers;
 
     public function __construct($option)
     {
         $this->option = $option;
+        $this->buildMatcher();
+    }
+
+    /**
+     * Builds matcher array
+     */
+    private function buildMatcher()
+    {
+        $this->matchers = [];
+        foreach ($this->option as $key => $value) {
+            if (is_callable($value)) {
+                $this->matchers[$key] = function($current, $row) use ($key, $value) {
+                    return $current && call_user_func($value, $row[$key], $row);
+                };
+            } else {
+                $this->matchers[$key] = function($current, $row) use ($key, $value) {
+                    return $current && ($row[$key] === $value);
+                };
+            }
+        }
     }
 
     /**
@@ -30,12 +55,8 @@ class Matcher implements MatcherInterface
     public function match($row)
     {
         $match = true;
-        foreach ($this->option as $key => $value) {
-            if (is_callable($value)) {
-                $match = $match && call_user_func($value, $row[$key], $row);
-            } else {
-                $match = $match && ($row[$key] === $value);
-            }
+        foreach ($this->matchers as $key => $matcher) {
+            $match = $matcher($match, $row);
         }
 
         return $match;
